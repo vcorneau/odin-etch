c14d99e3d507251be977fb3eafc795d08dffeedb
vcorneau <vcorneau@gmail.com>
2023-04-18 15:43:55 -0400

    Create a 10x10px div

---
+++ script.js
@@ -0,0 +1,23 @@
+const grid = document.getElementById("gridContainer");
+
+const cell = document.createElement("div")
+cell.className = "cell";
+cell.id = "cell";
+
+const row = document.createElement("div")
+row.className = "row";
+row.id = "row";
+
+//function makes a row of cells
+//function makeRow() {
+   
+    grid.appendChild(cell);
+
+//}
+
+//Make 16 rows of the cells
+/*function makeGrid() {
+    for (let i = 0; 1 < 16; i++) {
+        grid.makeRow;
+    }
+}*/
