14559c813dcd610c2e63248c14279f34f92a595d
vcorneau <vcorneau@gmail.com>
2023-04-18 16:09:24 -0400

    Add notes with next steps

--- script.js
+++ script.js
@@ -11,12 +11,10 @@ row.id = "row";
 //Create a row inside gridContainer
 grid.appendChild(row);
 
-//Make a cell inside of row x 16
+//Make a cell inside of row x 16 -- can I put this in a function and call x 16 rows?
 for(let i=0; i<16; i++) {
     row.appendChild(cell.cloneNode(true));
 }
-
-//Make a row of 16 cells
 
 
 //Make 16 rows of 16 cells
