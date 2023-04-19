79dc29d675a63e80d902b09d5befc13611b881c9
vcorneau <vcorneau@gmail.com>
2023-04-19 09:30:58 -0400

    Function to make 9 cells

--- script.js
+++ script.js
@@ -4,17 +4,26 @@ const cell = document.createElement("div")
 cell.className = "cell";
 cell.id = "cell";
 
-const row = document.createElement("div")
+const row = document.createElement("div");
 row.className = "row";
 row.id = "row";
 
-//Create a row inside gridContainer
-grid.appendChild(row);
 
-//Make a cell inside of row x 16 -- can I put this in a function and call x 16 rows?
-for(let i=0; i<16; i++) {
-    row.appendChild(cell.cloneNode(true));
+//Make 16 cells inside of row
+function makeRowCells() {
+    for(let i=0; i<3; i++) {
+        row.appendChild(cell.cloneNode(true));
+    }
 }
 
-
 //Make 16 rows of 16 cells
+function createGrid() {
+    for(let i=0; i<3; i++){
+        makeRowCells();
+        grid.appendChild(row);
+    }
+}
+
+createGrid();
+
+
