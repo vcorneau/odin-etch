611f3eb93b73a786a514e48c6f44b741acd13adb
vcorneau <vcorneau@gmail.com>
2023-04-19 09:47:46 -0400

    Add 16 rows of 16 cells

--- script.js
+++ script.js
@@ -11,16 +11,16 @@ row.id = "row";
 
 //Make 16 cells inside of row
 function makeRowCells() {
-    for(let i=0; i<3; i++) {
+    for(let i=0; i<16; i++) {
         row.appendChild(cell.cloneNode(true));
     }
 }
 
-//Make 16 rows of 16 cells
+//Make 16 rows of 16 cells. Right now, increases number of cells each row. Need to fix.
 function createGrid() {
-    for(let i=0; i<3; i++){
-        makeRowCells();
-        grid.appendChild(row);
+    makeRowCells();
+    for(let i=0; i<16; i++){
+        grid.appendChild(row.cloneNode(true));
     }
 }
 
